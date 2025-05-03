51f335ac0af725ca23dac56cdf2a77cddddc599c
kikiAlbrecher <christina.albrecher@gmx.at>
2025-05-03 03:03:39 +0200

    first commit

---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [CommonModule, HeroComponent, AboutMeComponent, SkillsComponent, ProjectsComponent, ReferencesComponent, ContactComponent],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
