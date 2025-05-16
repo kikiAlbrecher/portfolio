5475aef55cced960e929018073a9f55e5a8edf68
kikiAlbrecher <christina.albrecher@gmx.at>
2025-05-16 20:46:40 +0200

    added translations

--- src/app/main-content/hero/hero.component.ts
+++ src/app/main-content/hero/hero.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { MediaLinksComponent } from '../../media-links/media-links.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [TranslateModule, MediaLinksComponent],
+  imports: [CommonModule, TranslateModule, MediaLinksComponent],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss', 
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
 
--- src/app/main-content/projects/project-item/project-item.component.ts
+++ src/app/main-content/projects/project-item/project-item.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-project-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './project-item.component.html',
   styleUrl: './project-item.component.scss'
 })
