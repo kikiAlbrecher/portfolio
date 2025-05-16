b7bc1edc3c1084f9d64aadb8ecda431f573bbfaf
kikiAlbrecher <christina.albrecher@gmx.at>
2025-05-16 22:03:13 +0200

    added component 'skill-item' to skills

---
+++ src/app/main-content/skills/skill-item/skill-item.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './skill-item.component.html',
+  styleUrl: './skill-item.component.scss'
+})
+export class SkillItemComponent {
+  @Input() stackIconUrl!: string;
+  @Input() stackName!: string;
+}
--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,14 +1,62 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { SkillItemComponent } from './skill-item/skill-item.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, SkillItemComponent],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  i = 0;
 
+  stack = [
+    {
+      stackIconUrl: 'assets/icons/HTML.svg',
+      stackName: 'HTML',
+    },
+    {
+      stackIconUrl: 'assets/icons/CSS.svg',
+      stackName: 'CSS',
+    },
+    {
+      stackIconUrl: 'assets/icons/javaScript.svg',
+      stackName: 'JavaScript',
+    },
+    {
+      stackIconUrl: 'assets/icons/typeScript.svg',
+      stackName: 'TypeScript',
+    },
+    {
+      stackIconUrl: 'assets/icons/angular.svg',
+      stackName: 'Angular',
+    },
+    {
+      stackIconUrl: 'assets/icons/firebase.svg',
+      stackName: 'Firebase',
+    },
+    {
+      stackIconUrl: 'assets/icons/git.svg',
+      stackName: 'Git',
+    },
+    {
+      stackIconUrl: 'assets/icons/API.svg',
+      stackName: 'Rest API',
+    },
+    {
+      stackIconUrl: 'assets/icons/scrum.svg',
+      stackName: 'Scrum',
+    },
+    {
+      stackIconUrl: 'assets/icons/material-design.svg',
+      stackName: 'Material Design',
+    },
+    {
+      stackIconUrl: 'assets/icons/learning-ambition.svg',
+      stackName: 'Continually learning',
+    }
+  ];
 }
