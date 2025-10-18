6ceffbcd99b67903373c91e2f6ed7967367de21a
kikiAlbrecher <christina.albrecher@gmx.at>
2025-10-18 22:38:59 +0200

    Merge branch 'main' of https://github.com/kikiAlbrecher/leni-nav

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'leni-nav' title`, () => {
+  it(`should have the 'portfolio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('leni-nav');
+    expect(app.title).toEqual('portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, leni-nav');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portfolio');
   });
 });
